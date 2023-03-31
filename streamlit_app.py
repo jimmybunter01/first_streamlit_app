00a4855a817d9984515d20598b41c47c6faee4f2
jimmybunter01 <65281539+jimmybunter01@users.noreply.github.com>
2023-03-31 15:02:48 +0100

    Setting up better user choice to get info about food.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruit_choice = streamlit.text_input("What fruit would you like info about?", 'Kiwi')
+streamlit.write("The User entered ", fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalised = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalised)
