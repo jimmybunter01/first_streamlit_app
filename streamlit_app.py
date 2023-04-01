a6a9188847dcac3c0b2f3c4c6a499b37b432def9
jimmybunter01 <65281539+jimmybunter01@users.noreply.github.com>
2023-04-01 20:46:21 +0100

    Refining adding of data to Snowflake.

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,8 @@ if streamlit.button("Get Fruit Load List"):
     streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
-streamlit.text("Thanks for adding " + add_my_fruit)
-my_cur.execute("INSERT INTO fruit_load_list VALUES('from streamlit')")
+if streamlit.button("Add a Fruit to the List"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("INSERT INTO fruit_load_list VALUES('from streamlit')")
+    streamlit.text("Thanks for adding " + add_my_fruit)
