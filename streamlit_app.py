c84084a2360bebbd9517eb4f41eaeeb6ed8128f4
jimmybunter01 <65281539+jimmybunter01@users.noreply.github.com>
2023-04-01 19:04:37 +0100

    Added a text box which could be used to allow a user to add a fruit to the fruit list retrieved from Snowflake.

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,6 @@ my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = steamlit.text_input("What fruit would you like to add?")
+streamlit.txt("Thanks for adding " + add_my_fruit)
