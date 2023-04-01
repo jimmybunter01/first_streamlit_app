d3dab9ca6e6c0eab0ed9120337a7990c10b5e10d
jimmybunter01 <65281539+jimmybunter01@users.noreply.github.com>
2023-04-01 19:06:39 +0100

    Fixed thank you message.

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,4 +36,4 @@ streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
-streamlit.txt("Thanks for adding " + add_my_fruit)
+streamlit.text("Thanks for adding " + add_my_fruit)
